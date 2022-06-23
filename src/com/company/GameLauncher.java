56c139e800bcd1b1839175adf1ff4b038a2a9869
STANCU ANA-MARIA <maria.ana.stancu@outlook.com>
2022-06-23 00:24:06 -0900

    first commit

---
+++ src/com/company/GameLauncher.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+
+        GuessGame game=new GuessGame();
+        game.startGame();
+
+    }
+}
